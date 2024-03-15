129a5dab1c2311b942f7651ba74b571ea8d1ec71
dimassdoubles <codedimas@gmail.com>
2024-03-15 21:51:04 +0700

    feat: app card

---
+++ lib/features/common/presentation/widget/app_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:getasan_app/features/common/constant/style/app_colors.dart';
+import 'package:getasan_app/features/common/constant/style/app_texts.dart';
+import 'package:getasan_app/features/common/presentation/widget/gaps.dart';
+
+class AppCard extends StatelessWidget {
+  final String title;
+  final String icon;
+  final VoidCallback? onTap;
+  const AppCard({
+    super.key,
+    required this.title,
+    required this.icon,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(12.w),
+      child: Container(
+        padding: EdgeInsets.all(24.w),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.w),
+          color: AppColors.white,
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 3.h),
+              spreadRadius: 1,
+              blurRadius: 4,
+              color: Colors.black.withOpacity(0.25),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(
+                title,
+                style: AppTexts.extraBold.copyWith(
+                  color: AppColors.primary,
+                  fontSize: 20.sp,
+                ),
+              ),
+            ),
+            Gaps.h12,
+            Image.asset(icon),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/page/home_page.dart
+++ lib/features/home/presentation/page/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:getasan_app/features/common/constant/style/app_colors.dart';
+import 'package:getasan_app/features/common/presentation/widget/app_card.dart';
 import 'package:getasan_app/features/common/presentation/widget/button/primary_button.dart';
 import 'package:getasan_app/features/common/presentation/widget/button/secondary_button.dart';
 import 'package:getasan_app/features/common/presentation/widget/gaps.dart';
@@ -15,39 +16,49 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
-      body: Padding(
-        padding: EdgeInsets.all(16.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            PrimaryButton(
-              label: 'Primary Button',
-              onTap: () {},
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(16.w),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                PrimaryButton(
+                  label: 'Primary Button',
+                  onTap: () {},
+                ),
+                Gaps.v16,
+                PrimaryButton(
+                  label: 'Primary Button',
+                  backgroundColor: AppColors.secondary,
+                  onTap: () {},
+                ),
+                Gaps.v16,
+                SecondaryButton(
+                  label: 'Secondary Button',
+                  onTap: () {},
+                ),
+                Gaps.v16,
+                const TextInput(
+                  label: 'Nama Lengkap',
+                ),
+                Gaps.v16,
+                const TextInput(
+                  hint: 'Nama Kegiatan',
+                ),
+                Gaps.v16,
+                const DateInput(),
+                Gaps.v16,
+                const ImageInput(),
+                Gaps.v16,
+                const AppCard(
+                  title: 'Lapor Camat',
+                  icon: 'assets/icons/ic_lapor_camat.png',
+                ),
+                Gaps.v16,
+              ],
             ),
-            Gaps.v16,
-            PrimaryButton(
-              label: 'Primary Button',
-              backgroundColor: AppColors.secondary,
-              onTap: () {},
-            ),
-            Gaps.v16,
-            SecondaryButton(
-              label: 'Secondary Button',
-              onTap: () {},
-            ),
-            Gaps.v16,
-            const TextInput(
-              label: 'Nama Lengkap',
-            ),
-            Gaps.v16,
-            const TextInput(
-              hint: 'Nama Kegiatan',
-            ),
-            Gaps.v16,
-            const DateInput(),
-            Gaps.v16,
-            const ImageInput(),
-          ],
+          ),
         ),
       ),
     );
